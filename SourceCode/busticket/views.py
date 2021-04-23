f768db1fd366a18eb01ca35f4c4b01f35596477a
Serdar <serdar.mumcu@udemy.com>
2021-04-23 19:29:27 +0300

    Templates and Static Files and Basic Bootstrap are configured

--- SourceCode/busticket/views.py
+++ SourceCode/busticket/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World!")
+    my_dict = { 'insert_me' : "Now I am coming from busticket/index.html" }
+    return render(request,'busticket/index.html',context=my_dict)
